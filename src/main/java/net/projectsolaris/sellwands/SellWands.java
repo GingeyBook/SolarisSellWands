d76c251f37c9ecd8b5e3e4e7afda2c5820a55368
GingeyBook <gingeybook@projectsolaris.net>
2020-04-21 18:36:34 -0400

    Added /hoe command.
    Gives user a golden hoe

--- src/main/java/net/projectsolaris/sellwands/SellWands.java
+++ src/main/java/net/projectsolaris/sellwands/SellWands.java
@@ -1,23 +1,18 @@
 package net.projectsolaris.sellwands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SellWands extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Enabling SolarisSellWands by GingeyBook...");
+        this.getCommand("hoe").setExecutor(new giveHoe());
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
