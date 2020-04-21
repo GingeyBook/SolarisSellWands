db32b1a33c5e43b5d1f9eb8e16cb033ba6afaac3
GingeyBook <gingeybook@projectsolaris.net>
2020-04-21 18:15:14 -0400

    Initial Commit

---
+++ src/main/java/net/projectsolaris/sellwands/SellWands.java
@@ -0,0 +1,23 @@
+package net.projectsolaris.sellwands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SellWands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
